390e07e9272a069cb721cafaa82ca0bdaa0e909f
wwwshwww <www.shinderu.www@gmail.com>
2021-02-22 10:30:40 +0000

    Add modules for test

---
+++ server_modules/robot_server/client.py
@@ -0,0 +1,46 @@
+import grpc
+from robo_gym_server_modules.robot_server.grpc_msgs.python import robot_server_pb2, robot_server_pb2_grpc
+
+### copy this file to '/usr/local/lib/python3.6/dist-packages/robo_gym_server_modules/robot_server/'
+
+class Client():
+
+    def __init__(self, ip):
+
+        self.channel = grpc.insecure_channel(ip)
+        self.robot_server_stub = robot_server_pb2_grpc.RobotServerStub(self.channel)
+
+    def set_state(self, state):
+        # Old method, to be gradually replaced in all the stack
+        msg = self.robot_server_stub.SetState(robot_server_pb2.State(state = state), timeout = 6000)
+        return msg.success
+
+    def set_state_msg(self, state_msg):
+        msg = self.robot_server_stub.SetState(state_msg, timeout = 6000)
+        return msg.success
+
+    def get_state(self,):
+        # Old method, to be gradually replaced in all the stack
+        msg = self.robot_server_stub.GetState(robot_server_pb2.Empty(), timeout = 6000)
+        if msg.success == 1:
+            return msg.state
+        else:
+            raise Exception('Error while retrieving state')
+
+    def get_state_msg(self,):
+        msg = self.robot_server_stub.GetState(robot_server_pb2.Empty(), timeout = 6000)
+        if msg.success == 1:
+            return msg
+        else:
+            raise Exception('Error while retrieving state')
+
+    def send_action(self, action):
+        msg = self.robot_server_stub.SendAction(robot_server_pb2.Action(action = action), timeout = 6000 )
+        return msg.success
+
+    def send_action_get_state(self, action):
+        msg = self.robot_server_stub.SendActionGetState(robot_server_pb2.Action(action = action), timeout = 6000 )
+        if msg.success == 1:
+            return msg
+        else:
+            raise Exception('Error while retrieving state')
